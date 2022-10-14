6ea50a29e1f76fa02c013ed448dc2982a9cff10c
christofferaakre <christoffer.corfield@gmail.com>
2022-10-14 20:45:37 +0200

    finished firebase setup

--- firebase/index.ts
+++ firebase/index.ts
@@ -2,7 +2,11 @@ import admin from 'firebase-admin';
 import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
 import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
 
+const serviceAccount = require('../firebase-credentials.json');
+
 if(admin.apps.length === 0)
-    initializeApp();
+    initializeApp({
+        credential: cert(serviceAccount)
+    });
 
 export const db = getFirestore();

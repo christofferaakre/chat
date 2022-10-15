745a7e833171d8725c87e6228a98711d2dd12054
christofferaakre <christoffer.corfield@gmail.com>
2022-10-16 00:20:43 +0200

    added note about exposing api key

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,7 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
-// const firebaseConfig = require('../firebase-credentials.json');
+// NOTE: It is okay for the client to see
+// the firebase API key and this other information. In fact,
+// it is necessary. This is not like the API key for many other services
+// where it must be kept secret. Exposing this information to the client
+// is 100% fine and necessary.
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
